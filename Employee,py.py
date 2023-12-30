54353038e996bdb72376b0ef9831d9bb4c4c297e
Bhargavi31029 <154216489+Bhargavi31029@users.noreply.github.com>
2023-12-30 11:25:42 +0530

    Add files via upload

---
+++ Employee,py.py
@@ -0,0 +1,17 @@
+class Employee:
+    company ='TESLA'
+    ceo ='ELON MUSK'
+    
+    def insert_member(self,name,age,sal,eid):
+        self.name=name
+        self.age=age
+        self.salary=salary
+        self.eid=eid
+
+        
+bhanu=Employee()
+vinitha=Employee()
+
+Employee.insert_member(bhanu,'bhanu',20,1000000,555555)
+Employee.insert_member(vinitha,'vinitha',20,1000000,555555)
+
